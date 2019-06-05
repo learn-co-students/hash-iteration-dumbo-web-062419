826cb233265722cf1277c4937bbefd8f315dc8cb
Olga Rosas <olga.rosas@gmail.com>
2019-06-05 17:56:55 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,8 +1,8 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
@@ -10,5 +10,5 @@ def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |key, value|
     passenger.select if key == suite_a
-    
+
 end
